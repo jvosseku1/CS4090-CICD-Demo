1a2f133d82b431f9d37a1204234a9c5e1f4e7f3f
jvosseku1 <jacobvossekuil@gmail.com>
2025-12-12 16:47:44 +0000

    Added subtract, multiply, and divide functions and tests

--- test/test_app.py
+++ test/test_app.py
@@ -4,10 +4,39 @@ from pathlib import Path
 root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root/"src"))
 
-from app import add
+import app
 
 def test_add():
     assert add(5, 6) == 11
 
 def test_add2():
     assert add(5, 6) != 10
+
+def test_add3():
+    assert add(-5, 1) == -4
+
+def test_sub1():
+    assert sub(6,5) == 1
+
+def test_sub2():
+    assert sub(5,6) == -1
+
+def test_mult1():
+    assert mult(10,10) == 100
+
+def test_mult2():
+    assert mult(-1, 10) == -10
+
+def test_mult3():
+    assert mult(-1, -10) == 10
+
+def test_div1():
+    if b == 0:
+        raise ValueError("b cannot be 0\n nothing divisible by 0")
+
+def test_div2():
+    assert div(10/2) == 5
+
+def test_div3():
+    assert div(100/10) == 10
+
